7bc8a84701dac090cc0668b80114b0c29ad63357
impactice <107308111+impactice@users.noreply.github.com>
2026-03-17 08:47:14 +0900

    Add files via upload

---
+++ 0303/week1-1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val input: String? = null
+    println(input?.length ?: 0)
+
+    val language: String = "Kotlin"
+    //language = "java";
+    var year: Int = 2013
+    year = 2024
+    var age: Int
+    println("$language is $year")
+}
