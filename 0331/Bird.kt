bba22d1c677aabc41884d8a23d2a0c151e839bf3
impactice <107308111+impactice@users.noreply.github.com>
2026-03-31 11:31:25 +0900

    Add files via upload

--- 0331/Bird.kt
+++ 0331/Bird.kt
@@ -5,6 +5,14 @@ class Bird {
     var beak:String = "short"
     var color:String = "blue"
 
+    // ① 초기화 블록
+    init {
+        println("----------초기화 블록 시작----------")
+        println("이름은 $name, 부리는 $beak")
+        this.sing(3)
+        println("---------- 초기화 블록 끝 ----------")
+    }
+
     //secondary constructor
     constructor(name:String, wing:Int, beak:String, color:String) {
         this.name = name
@@ -13,6 +21,11 @@ class Bird {
         this.color = color
     }
 
+    constructor(name:String, beak:String) {
+        this.name = name
+        this.beak = beak
+        this.color = "yellow"
+    }
     //method
     fun fly() = println("Fly wing : $wing")
     fun sing(vol:Int) = println("Singing $vol")
@@ -20,9 +33,13 @@ class Bird {
 fun main() {
     val coco = Bird(name = "Coco", wing = 2, beak = "short",color = "red")
     //coco.color = "red"
+    //val cozy = Bird("cozy", beak = "Long")
 
     println("coco.color : ${coco.color}")
+    //println("cozy.color : ${cozy.color}")
     coco.fly()
     coco.sing(10)
-
+    //cozy.fly()
+    //cozy.sing(10)
+    val bird1 = Bird(beak="Long",color = "black")
 }
